5a15b762579aafbaff048ba397b1965a8f4e5ab3
Michael Ndjinga <michael.ndjinga@cea.fr>
2017-10-12 19:16:32 +0200

    Création fichier d'environnement env_CDMATH.sh Remplissage du fichier d'environnement dans le CMakelist.txt

---
+++ env_CDMATH.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CDMATH_DIR=@CMAKE_INSTALL_PREFIX@
+export PETSC_DIR=@PETSC_DIR@
+export PETSC_ARCH="@PETSC_ARCH@"
+
+export LD_LIBRARY_PATH=$CDMATH_DIR/lib/:$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:${LD_LIBRARY_PATH}
+export PYTHONPATH=$CDMATH_DIR/lib/:$CDMATH_DIR/lib/cdmath:$CDMATH_DIR/bin/cdmath:${PYTHONPATH}
+
