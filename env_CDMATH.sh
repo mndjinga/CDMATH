6de5ea4b23e0750feac680e3859f2ee8cd9b4a62
Michael Ndjinga <michael.ndjinga@cea.fr>
2018-01-05 13:59:23 +0100

    Updated the pythonpath variable for tests and examples

--- env_CDMATH.sh
+++ env_CDMATH.sh
@@ -4,6 +4,6 @@ export CDMATH_DIR=@CMAKE_INSTALL_PREFIX@
 export PETSC_DIR=@PETSC_DIR@
 export PETSC_ARCH="@PETSC_ARCH@"
 
-export LD_LIBRARY_PATH=$CDMATH_DIR/lib/:$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:${LD_LIBRARY_PATH}
-export PYTHONPATH=$CDMATH_DIR/lib/:$CDMATH_DIR/lib/cdmath:$CDMATH_DIR/bin/cdmath:${PYTHONPATH}
+export LD_LIBRARY_PATH=$CDMATH_DIR/lib/:$CDMATH_DIR/lib/medcoupling:$CDMATH_DIR/lib/med:$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:${LD_LIBRARY_PATH}
+export PYTHONPATH=$CDMATH_DIR/lib/:$CDMATH_DIR/lib/cdmath:$CDMATH_DIR/lib/medcoupling:$CDMATH_DIR/lib/med:$CDMATH_DIR/bin/cdmath:${PYTHONPATH}
 
