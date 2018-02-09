4d3869ac86eb4bceac25492ccb51a021ae1076e1
Michael Ndjinga <michael.ndjinga@cea.fr>
2018-02-09 14:49:50 +0100

    Improved VTK_routines.py

--- postprocessing/VTK_routines.py
+++ postprocessing/VTK_routines.py
@@ -22,26 +22,13 @@ def Extract_VTK_data_over_line_to_csv_file(inputFileName, outputFileName,
     cutter.SetInputConnection(reader.GetOutputPort())
     cutter.Update()
 
-    w=vtk.vtkPolyDataWriter()
-    w.SetFileName(outputFileName)
-    w.SetInputConnection(cutter.GetOutputPort())
-    w.Write() 
-
-    data = cutter.GetOutput()
-
-    triangles = data.GetPolys().GetData()
-    points = data.GetPoints()
-
-    mapper = vtk.vtkCellDataToPointData()
-    mapper.AddInputData(data)
-    mapper.Update()
-
+    SaveData('outputFileName', proxy=cutter)
     
-def Extract_MED_data_over_line_to_numpyArray(inputFileName, point1, point2, resolution):
-    pvs._DisableFirstRenderCameraReset()
-    data_vtu = MEDReader.Reader(FileName=[inputFileName])
-    PlotOverLine1 = pvs.PlotOverLine(Source="High Resolution Line Source"
-                                     )
+def Extract_VTK_data_over_line_to_numpyArray(inputFileName, point1, point2, resolution):
+
+    data_vtu = vtk.vtkUnstructuredGridReader(FileName=[inputFileName])
+
+    PlotOverLine1 = pvs.PlotOverLine(data_vtu, Source="High Resolution Line Source" )
     PlotOverLine1.Source.Point1 = point1
     PlotOverLine1.Source.Point2 = point2
     PlotOverLine1.Source.Resolution = resolution
@@ -52,17 +39,17 @@ def Extract_MED_data_over_line_to_numpyArray(inputFileName, point1, point2, reso
     return numpy_array
     
 def Extract_field_data_over_line_to_numpyArray(field, point1, point2, resolution):
-    field.writeMED(field.get_name())
-    inputFileName = os.getcwd()+field.get_name()+".med"
+    field.writeVTK(field.get_name())
+    inputFileName = os.getcwd()+field.get_name()+".vtu"
 
-    result = Extract_MED_data_over_line_to_numpyArray(inputFileName, point1, point2, resolution)
+    result = Extract_VTK_data_over_line_to_numpyArray(inputFileName, point1, point2, resolution)
 
     os.remove(inputFileName)
     return result
 
-def Slice_MED_data_over_line_to_numpyArray(inputFileName, point, normal):
-    pvs._DisableFirstRenderCameraReset()
-    data_vtu = MEDReader.Reader(FileName=[inputFileName])
+def Slice_VTK_to_numpyArray(inputFileName, point, normal):
+
+    data_vtu = vtk.vtkUnstructuredGridReader(FileName=[inputFileName])
     Slice1 = pvs.Slice(SliceType="Plane")
     Slice1.SliceOffsetValues = [0.0]
     Slice1.SliceType.Origin = point
@@ -78,8 +65,8 @@ def Slice_MED_data_over_line_to_csv_file(inputFileName,
                                         outputFileName,
                                         point,
                                         normal):
-    pvs._DisableFirstRenderCameraReset()
-    data_vtu = MEDReader.Reader(FileName=[inputFileName])
+
+    data_vtu = vtk.vtkUnstructuredGridReader(FileName=[inputFileName])
     Slice1 = pvs.Slice(SliceType="Plane")
     Slice1.SliceOffsetValues = [0.0]
     Slice1.SliceType.Origin = point
@@ -94,18 +81,18 @@ def Slice_field_data_over_line_to_numpyArray(field,
                                         outputFileName,
                                         point,
                                         normal):
-    field.writeMED(field.get_name())
-    inputFileName = os.getcwd()+field.get_name()+".med"
+    field.writeVTK(field.get_name())
+    inputFileName = os.getcwd()+field.get_name()+".vtu"
  
-    result = Slice_MED_data_over_line_to_numpyArray(inputFileName, point1, point2, resolution)
+    result = Slice_VTK_to_numpyArray(inputFileName, point1, point2, resolution)
 
     os.remove(inputFileName)
     return result
 
-def Save_MED_to_picture_file(inputFileName,
+def Save_VTK_to_picture_file(inputFileName,
                              outputFileName,
                              ):
-    data_vtu = MEDReader.Reader(FileName=[inputFileName])
+    data_vtu = vtk.vtkUnstructuredGridReader(FileName=[inputFileName])
     Show(data_vtu)
     Render()
     SaveScreenshot(outputFileName) 
