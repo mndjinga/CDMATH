5cc9418fbfe1af3dd6a196ce8fc01187cbdc0bdd
Michael Ndjinga <michael.ndjinga@cea.fr>
2018-05-17 16:53:32 +0200

    Debugging first validation test

--- validation/validation.py
+++ validation/validation.py
@@ -3,6 +3,6 @@ import FiniteElements2DWithCDMATH
 error_tab=[0]*5
 mesh_size_tab=[0]*5
 i=0
-for filename in [triangleMeshSquare_1,triangleMeshSquare_2,triangleMeshSquare_3,triangleMeshSquare_4,triangleMeshSquare_5]:
-    error_tab[i] =FiniteElements2DWithCDMATH(filename)
+for filename in ['triangleMeshSquare_1','triangleMeshSquare_2','triangleMeshSquare_3','triangleMeshSquare_4','triangleMeshSquare_5']:
+    error_tab[i] =FiniteElements2DWithCDMATH.FiniteElements2DWithCDMATH(filename)
     mesh_size_tab[i]=cdmath.Mesh(filename+"med").getNumberOfNodes()
