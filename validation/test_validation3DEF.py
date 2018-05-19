90b7754d44b5b91ecbb12534e334b9dbdadd9591
Michael Ndjinga <michael.ndjinga@cea.fr>
2018-05-19 03:56:11 +0200

    Renamed validation test cases in order to prepare use of pytest. Use of assert to control the validity of the convergence order

---
+++ validation/test_validation3DEF.py
@@ -0,0 +1,48 @@
+import cdmath
+import FiniteElements3DWithCDMATH
+import matplotlib.pyplot as plt
+import numpy as np
+from math import log10
+
+def test_validation3DEF():
+    #### 3D FE tetrahedra mesh
+    nbMeshes=7
+    error_tab=[0]*nbMeshes
+    mesh_size_tab=[0]*nbMeshes
+    mesh_path='../validation/3DTetrahedra/'
+    mesh_name='meshCubeWithTetrahedraFE'
+    i=0
+    # Computation of the numerical error
+    for filename in ['meshCubeTetrahedra_0','meshCubeTetrahedra_1','meshCubeTetrahedra_2','meshCubeTetrahedra_3','meshCubeTetrahedra_4','meshCubeTetrahedra_5','meshCubeTetrahedra_6']:
+        error_tab[i], mesh_size_tab[i] =FiniteElements3DWithCDMATH.solve(mesh_path+filename)
+        error_tab[i]=log10(error_tab[i])
+        mesh_size_tab[i] = log10(mesh_size_tab[i])
+        i=i+1
+        
+    # Least square linear regression
+    # Find the best a,b such that f(x)=ax+b best approximates the convergence curve
+    # The vector X=(a,b) solves a symmetric linear system AX=B with A=(a1,a2\\a2,a3), B=(b1,b2)
+    a1=np.dot(mesh_size_tab,mesh_size_tab)
+    a2=np.sum(mesh_size_tab)
+    a3=nbMeshes
+    b1=np.dot(error_tab,mesh_size_tab)   
+    b2=np.sum(error_tab)
+    
+    det=a1*a3-a2*a2
+    a=( a3*b1-a2*b2)/det
+    b=(-a2*b1+a1*b2)/det
+    
+    print "FE on 3D tetrahedral mesh : scheme order is ", -a
+    assert abs(a+0.329)<0.1
+    
+    # Plot of figures
+    plt.plot(mesh_size_tab, error_tab, label='log(|numerical-exact|)')
+    plt.plot(mesh_size_tab, a*np.array(mesh_size_tab)+b,label='least square slope : '+'%.3f' % a)
+    plt.legend()
+    plt.xlabel('log(number of nodes)')
+    plt.ylabel('log(error)')
+    plt.title('Convergence of finite elements for Laplace operator on a 3D tetrahedral mesh')
+    plt.savefig(mesh_name+".png")
+
+if __name__ == """__main__""":
+    test_validation3DEF()
---
+++ validation/test_validation3DVF_s.py
@@ -0,0 +1,49 @@
+import cdmath
+import FiniteVolumes3DWithCDMATH
+import matplotlib.pyplot as plt
+import numpy as np
+from math import log10
+
+def test_validation3DVF_s():
+    ### 3D FV rectangular mesh
+    nbMeshes=3
+    error_tab=[0]*nbMeshes
+    mesh_size_tab=[0]*nbMeshes
+    mesh_name='meshCubeWithCuboids3DFV'
+    i=0
+    # Computation of the numerical error
+    for nx in [11,21,41]:
+        my_mesh=cdmath.Mesh(0,1,nx,0,1,nx,0,1,nx)
+        error_tab[i], mesh_size_tab[i] =FiniteVolumes3DWithCDMATH.solve(my_mesh,str(nx)+'x'+str(nx)+'x'+str(nx))
+        error_tab[i]=log10(error_tab[i])
+        mesh_size_tab[i] = log10(mesh_size_tab[i])
+        i=i+1
+        
+    # Least square linear regression
+    # Find the best a,b such that f(x)=ax+b best approximates the convergence curve
+    # The vector X=(a,b) solves a symmetric linear system AX=B with A=(a1,a2\\a2,a3), B=(b1,b2)
+    a1=np.dot(mesh_size_tab,mesh_size_tab)
+    a2=np.sum(mesh_size_tab)
+    a3=nbMeshes
+    b1=np.dot(error_tab,mesh_size_tab)   
+    b2=np.sum(error_tab)
+    
+    det=a1*a3-a2*a2
+    assert det!=0, 'test_validation3DVF_s() : Make sure you use distinct meshes'
+    a=( a3*b1-a2*b2)/det
+    b=(-a2*b1+a1*b2)/det
+    
+    print "FV on 3D rectangular parallelepiped mesh : scheme order is ", -a
+    assert abs(a+0.668)<0.1
+    
+    # Plot of figures
+    plt.plot(mesh_size_tab, error_tab, label='log(|numerical-exact|)')
+    plt.plot(mesh_size_tab, a*np.array(mesh_size_tab)+b,label='least square slope : '+'%.3f' % a)
+    plt.legend()
+    plt.xlabel('log(number of nodes)')
+    plt.ylabel('log(error)')
+    plt.title('Convergence of finite volumes for Laplace operator on a 3D rectangular mesh')
+    plt.savefig(mesh_name+".png")
+
+if __name__ == """__main__""":
+    test_validation3DVF_s()
