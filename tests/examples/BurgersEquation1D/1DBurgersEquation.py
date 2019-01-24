4ceb3fc42dcabdb15082183c0ae337a2472c306c
Michael Ndjinga <ndjinga@localhost.localdomain>
2019-01-24 20:56:24 +0100

    Renamed test as it will use several numerical methods

--- tests/examples/BurgersEquation1D/1DBurgersEquation.py
+++ tests/examples/BurgersEquation1D/1DBurgersEquation.py
@@ -20,14 +20,26 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 import sys
 
+def Flux_Godunov(u_l, u_r):
+    if (u_l==u_r):
+        flux = 0.5*u_l*u_l
+    elif (u_l<0 and 0<u_r):
+        fluxs = 0.;
+    elif (u_l<u_r):
+        flux = min(0.5*u_l*u_l,0.5*u_r*u_r);
+    elif (u_l>u_r):
+        flux = max(0.5*u_l*u_l,0.5*u_r*u_r);
+    return flux
+
 def Burgers1D():
     ##################### Simulation parameters
     a = 0.0 # space domain :  a <= x <= b
     b = 1.
-    nx=1000
+    nx=200
     dx = (b - a) / nx #space step
 
     tmax = 0.9 # runs the simulation for 0 <= t <= tMax
+    ntmax=1000
     cfl=0.95
 
     x=[a+0.5*dx + i*dx for i in range(nx)]   # array of cell center (1D mesh)
@@ -35,8 +47,8 @@ def Burgers1D():
     ########################## Initial data
     xa=0.1
     xb=0.5
-    U_initial = [ (xi<xa)+(xa<=xi)*(xi<=xb)*(np.cos(np.pi*(xa-xi)/(xa-xb))+1.0)*0.5  for xi in x];# to be used with a=0, b=1
-    Un        = [ (xi<xa)+(xa<=xi)*(xi<=xb)*(np.cos(np.pi*(xa-xi)/(xa-xb))+1.0)*0.5  for xi in x];# to be used with a=0, b=1
+    u_initial = [ (xi<xa)+(xa<=xi)*(xi<=xb)*(np.cos(np.pi*(xa-xi)/(xa-xb))+1.0)*0.5  for xi in x];# to be used with a=0, b=1
+    u_godunov        = [ (xi<xa)+(xa<=xi)*(xi<=xb)*(np.cos(np.pi*(xa-xi)/(xa-xb))+1.0)*0.5  for xi in x];# to be used with a=0, b=1
     Unp1        = [ (xi<xa)+(xa<=xi)*(xi<=xb)*(np.cos(np.pi*(xa-xi)/(xa-xb))+1.0)*0.5  for xi in x];# to be used with a=0, b=1
 
     max_initial=max(u_initial)
@@ -59,50 +71,41 @@ def Burgers1D():
         plt.xlim(a,b)
         plt.ylim( min_initial - 0.1*(max_initial-min_initial), max_initial +  0.1*(max_initial-min_initial) )
         plt.title('Finite folume schemes for Burgers equation')
-        line1, = plt.plot(x, u, label='Godunov scheme') #new picture for video # Returns a tuple of line objects, thus the comma
+        line1, = plt.plot(x, u_godunov, label='Godunov scheme') #new picture for video # Returns a tuple of line objects, thus the comma
     
         print("Starting time loop")
-        print("-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt))
-        np.savetxt("BurgersEquation_FVGodunov_ResultField_0"+".txt", u, delimiter="\n")
+        print("-- Iter: " + str(it) + ", Time: " + str(time) )
+        np.savetxt("BurgersEquation_FVGodunov_ResultField_0"+".txt", u_godunov, delimiter="\n")
         writer.grab_frame()
         plt.savefig("BurgersEquation_FVGodunov_ResultField_0"+".png")
 
         ############################# Time loop
         while (it < ntmax and time <= tmax):
-            upw = max(abs(u))
+            upw = max(np.abs(u_godunov))
             dt = cfl * dx / upw
-            flux_L = 0.5*u[0]*u[0]#Flux in the ghost cell
-            flux_R = 0.5*u[nx-1]*u[nx-1]#Flux in the ghost cell
             # Loop on all cells
             for i in xrange(0,nx):
                 if (i==0):
-                    flux_iminus = flux_L;
+                    flux_iminus = 0.5*u_godunov[0]*u_godunov[0]#Flux at the left Neumann boundary
                 if (i==nx-1):
-                    flux_iplus  = flux_R;
+                    flux_iplus  = 0.5*u_godunov[nx-1]*u_godunov[nx-1]#Flux at the right Neumann boundary
                 else:
-                    if (Un[i]==Un[i+1]):
-                        flux_iplus = 0.5*Un[i]*Un[i];
-                    elif (Un[i]<0 and 0<Un[i+1]):
-                        flux_iplus = 0.;
-                    elif (Un[i]<Un[i+1]):
-                        flux_iplus = min(0.5*Un[i]*Un[i],0.5*Un[i+1]*Un[i+1]);
-                    elif (Un[i]>Un[i+1]):
-                        flux_iplus = max(0.5*Un[i]*Un[i],0.5*Un[i+1]*Un[i+1]);
+                    flux_iplus = Flux_Godunov(u_godunov[i],u_godunov[i+1])
                 pass
-                Unp1[i] = Un[i] - dt/dx*(flux_iplus-flux_iminus);
+                Unp1[i] = u_godunov[i] - dt/dx*(flux_iplus-flux_iminus);
                 flux_iminus = flux_iplus;
             pass
-            Un = Unp1
+            u_godunov = Unp1
     
             time += dt
             it += 1
 
             # Postprocessing
-            line1.set_ydata(Un)
+            line1.set_ydata(u_godunov)
             writer.grab_frame()
             if (it % output_freq == 0):
                 print("-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt))
-                np.savetxt( "BurgersEquation_FVGodunov_ResultField_"+str(it)+".txt", Un, delimiter="\n")
+                np.savetxt( "BurgersEquation_FVGodunov_ResultField_"+str(it)+".txt", u_godunov, delimiter="\n")
                 plt.savefig("BurgersEquation_FVGodunov_ResultField_"+str(it)+".png")
                 #plt.show()
     
