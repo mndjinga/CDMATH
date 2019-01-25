98fe9dc103753f051bf148a91a2461d4dd9cfad2
Michael Ndjinga <michael.ndjinga@cea.fr>
2019-01-25 16:28:09 +0100

    Adjusted legend position

--- tests/examples/BurgersEquation1D/1DBurgersEquation.py
+++ tests/examples/BurgersEquation1D/1DBurgersEquation.py
@@ -81,7 +81,7 @@ def Burgers1D():
         plt.xlabel('x')
         plt.ylabel('u')
         plt.xlim(a,b)
-        plt.ylim( min_initial - 0.1*(max_initial-min_initial), max_initial +  0.1*(max_initial-min_initial) )
+        plt.ylim( min_initial - 0.1*(max_initial-min_initial), max_initial +  0.3*(max_initial-min_initial) )
         plt.title('Finite volume schemes for Burgers equation')
         line1, = plt.plot(x, u_godunov, label='Godunov scheme') #new picture for video # Returns a tuple of line objects, thus the comma
         line2, = plt.plot(x, u_ncsv,    label='Non conservative scheme') #new picture for video # Returns a tuple of line objects, thus the comma
