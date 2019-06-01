b4b89015588ad493e00ae5c74b3843075e7f5e8c
Michael Ndjinga <michael.ndjinga@cea.fr>
2019-06-01 19:34:25 +0200

    Created separate tests for upwind and  staggered schemes

--- tests/examples/WaveSystem1DStaggered_RiemannProblem/WaveSystem1DStaggered_RiemannProblem.py
+++ tests/examples/WaveSystem1DStaggered_RiemannProblem/WaveSystem1DStaggered_RiemannProblem.py
@@ -46,49 +46,6 @@ def jacobianMatrices(coeff,scaling):
        
     return A,absA
     
-    
-def computeUpwindDivergenceMatrix(a,b,nx,nbVoisinsMax,dt,scaling):
-    nbCells = nx
-    dx=(b-a)/nx
-    dim=1
-    nbComp=dim+1
-    normal=cdmath.Vector(dim)
-
-    implMat=cdmath.SparseMatrixPetsc(nbCells*nbComp,nbCells*nbComp,(nbVoisinsMax+1)*nbComp)
-
-    A,absA= jacobianMatrices(dt/dx,scaling)
-    for j in range(nbCells):#On parcourt les cellules
-        if ( j==0) :
-            implMat.addValue(j*nbComp,(j+1)*nbComp,(A-absA)/2)
-            implMat.addValue(j*nbComp,    j*nbComp,(A-absA)/2*(-1.))
-        elif ( j==nbCells-1) :
-            implMat.addValue(j*nbComp,    j*nbComp,(A+absA)/2)
-            implMat.addValue(j*nbComp,(j-1)*nbComp,(A+absA)/2*(-1.))
-        else :
-            implMat.addValue(j*nbComp,(j+1)*nbComp,(A-absA)/2)
-            implMat.addValue(j*nbComp,    j*nbComp,(A-absA)/2*(-1.))
-
-            implMat.addValue(j*nbComp,    j*nbComp,(A+absA)/2)
-            implMat.addValue(j*nbComp,(j-1)*nbComp,(A+absA)/2*(-1.))
-                
-    return implMat
-
-def staggeredMatrices(coeff,scaling):
-    dim=1
-    S1=cdmath.Matrix(dim+1,dim+1)
-    S2=cdmath.Matrix(dim+1,dim+1)
-
-    for i in range(dim):
-        if( scaling==0):
-            S1[0,i+1]=c0*c0*coeff
-            S2[i+1,0]=      coeff
-        else:
-            S1[0,i+1]=   c0*coeff
-            S2[i+1,0]=   c0*coeff
-       
-    return A,absA
-    
-    
 def computeStaggeredDivergenceMatrix(a,b,nx,nbVoisinsMax,dt,scaling):
     nbCells = nx
     dx=(b-a)/nx
@@ -128,92 +85,52 @@ def WaveSystemVF(ntmax, tmax, cfl, a,b,nx, output_freq, meshName,scaling):
     dt = cfl * dx / c0
 
     nbVoisinsMax=2
-    isImplicit=False
     
     #iteration vectors
-    Un =cdmath.Vector(nbCells*(dim+1))
-    dUn=cdmath.Vector(nbCells*(dim+1))
-    
     Un_staggered =cdmath.Vector(nbCells*(dim+1))
     dUn_staggered=cdmath.Vector(nbCells*(dim+1))
-
-    Un_implicite =cdmath.Vector(nbCells*(dim+1))
-    dUn_implicite=cdmath.Vector(nbCells*(dim+1))
     
     # Initial conditions #
     print("Construction of the initial condition …")
-    pressure_field, velocity_field = initial_conditions_Riemann_problem(a,b,nx)
     pressure_field_staggered, velocity_field_staggered = initial_conditions_Riemann_problem(a,b,nx)
-    pressure_field_implicite, velocity_field_implicite = initial_conditions_Riemann_problem(a,b,nx)
-    max_initial_p=max(pressure_field)
-    min_initial_p=min(pressure_field)
-    max_initial_v=max(velocity_field)
-    min_initial_v=min(velocity_field)
+    max_initial_p=max(pressure_field_staggered)
+    min_initial_p=min(pressure_field_staggered)
+    max_initial_v=max(velocity_field_staggered)
+    min_initial_v=min(velocity_field_staggered)
     
     for k in range(nbCells):
-        Un[k*(dim+1)+0] =     pressure_field[k]
-        Un[k*(dim+1)+1] =rho0*velocity_field[k]
         Un_staggered[k*(dim+1)+0] =     pressure_field_staggered[k]
         Un_staggered[k*(dim+1)+1] =rho0*velocity_field_staggered[k]
-        Un_implicite[k*(dim+1)+0] =     pressure_field_implicite[k]
-        Un_implicite[k*(dim+1)+1] =rho0*velocity_field_implicite[k]
 
     # Video settings
     FFMpegWriter = manimation.writers['ffmpeg']
     metadata = dict(title="Finite volumes schemes for the 2D Wave System", artist = "CEA Saclay", comment="Shock propagation")
     writer=FFMpegWriter(fps=10, metadata=metadata, codec='h264')
-    with writer.saving(plt.figure(), "2DWaveSystem_FV"+".mp4", ntmax):
+    with writer.saving(plt.figure(), "2DWaveSystem_Staggered"+".mp4", ntmax):
         #sauvegarde de la donnée initiale
         plt.xlabel('x (m)')
         plt.ylabel('Pressure -Pa)')
         plt.xlim(a,b)
         plt.ylim( min_initial_p - 0.1*(max_initial_p-min_initial_p), max_initial_p +  0.1*(max_initial_p-min_initial_p) )
         plt.title("Riemann problem for Wave system on " + str(nx) + " cells")
-        line1, = plt.plot([a+0.5*dx + i*dx for i in range(nx)], pressure_field, label='Explicit upwind scheme') #new picture for video # Returns a tuple of line objects, thus the comma
         line2, = plt.plot([a+0.5*dx + i*dx for i in range(nx)], pressure_field_staggered, label='Staggered scheme') #new picture for video # Returns a tuple of line objects, thus the comma
-        line3, = plt.plot([a+0.5*dx + i*dx for i in range(nx)], pressure_field_implicite, label='Implicit upwind scheme') #new picture for video # Returns a tuple of line objects, thus the comma
         plt.legend()
         writer.grab_frame()
-        plt.savefig("WaveSystem"+str(dim)+"DUpwindExplicit"+meshName+"_pressure"+"_0"+".png")
-        np.savetxt( "WaveSystem"+str(dim)+"DUpwindExplicit"+meshName+"_pressure"+"_0"+".txt", pressure_field, delimiter="\n")
-        np.savetxt( "WaveSystem"+str(dim)+"DUpwindExplicit"+meshName+"_velocity"+"_0"+".txt", velocity_field, delimiter="\n")
         plt.savefig("WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure"+"_0"+".png")
-        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure"+"_0"+".txt", pressure_field, delimiter="\n")
-        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_velocity"+"_0"+".txt", velocity_field, delimiter="\n")
-        plt.savefig("WaveSystem"+str(dim)+"DUpwindImplicit"+meshName+"_pressure"+"_0"+".png")
-        np.savetxt( "WaveSystem"+str(dim)+"DUpwindImplicit"+meshName+"_pressure"+"_0"+".txt", pressure_field, delimiter="\n")
-        np.savetxt( "WaveSystem"+str(dim)+"DUpwindImplicit"+meshName+"_velocity"+"_0"+".txt", velocity_field, delimiter="\n")
+        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure"+"_0"+".txt", pressure_field_staggered, delimiter="\n")
+        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_velocity"+"_0"+".txt", velocity_field_staggered, delimiter="\n")
         
-        divMat=computeUpwindDivergenceMatrix(a,b,nx,nbVoisinsMax,dt,scaling)
         divMat_staggered=computeStaggeredDivergenceMatrix(a,b,nx,nbVoisinsMax,dt,scaling)
-        divMat_implicit=computeUpwindDivergenceMatrix(a,b,nx,nbVoisinsMax,dt,scaling)
             
         iterGMRESMax=50
-    
-        divMat_implicit.diagonalShift(1)#only after  filling all coefficients
-        if( scaling==0):
-            LS=cdmath.LinearSolver(divMat_implicit,Un,iterGMRESMax, precision, "GMRES","ILU")
-        else:
-            LS=cdmath.LinearSolver(divMat_implicit,Vn,iterGMRESMax, precision, "GMRES","ILU")
     
         divMat_staggered.diagonalShift(1)#only after  filling all coefficients
         LS_staggered=cdmath.LinearSolver(divMat_staggered,Un_staggered,iterGMRESMax, precision, "GMRES","ILU")
     
-        print("Starting computation of the linear wave system with upwind and staggered scheme …")
+        print("Starting computation of the linear wave system with staggered scheme …")
         
         # Starting time loop
         while (it<ntmax and time <= tmax and not isStationary):
-            dUn=divMat*Un
-            Un-=dUn
-    
-            dUn_implicite=Un_implicite.deepCopy()
-            LS.setSndMember(Un_implicite)
-            Un_implicite=LS.solve();
-            if(not LS.getStatus()):
-                print "Linear system did not converge ", LS.getNumberOfIter(), " GMRES iterations"
-                raise ValueError("Pas de convergence du système linéaire");
-            dUn_implicite-=Un_implicite
-    
             dUn_staggered=Un_staggered.deepCopy()
             LS_staggered.setSndMember(Un_staggered)
             Un_staggered=LS_staggered.solve();
@@ -223,16 +140,10 @@ def WaveSystemVF(ntmax, tmax, cfl, a,b,nx, output_freq, meshName,scaling):
             dUn_staggered-=Un_staggered
     
             for k in range(nbCells):
-                pressure_field[k] = Un[k*(dim+1)+0]
-                velocity_field[k] = Un[k*(dim+1)+1] / rho0
                 pressure_field_staggered[k] = Un_staggered[k*(dim+1)+0]
                 velocity_field_staggered[k] = Un_staggered[k*(dim+1)+1] / rho0
-                pressure_field_implicite[k] = Un_implicite[k*(dim+1)+0]
-                velocity_field_implicite[k] = Un_implicite[k*(dim+1)+1] / rho0
     
-            line1.set_ydata(pressure_field)
             line2.set_ydata(pressure_field_staggered)
-            line3.set_ydata(pressure_field_implicite)
             writer.grab_frame()
     
             time=time+dt;
@@ -241,16 +152,11 @@ def WaveSystemVF(ntmax, tmax, cfl, a,b,nx, output_freq, meshName,scaling):
             #Sauvegardes
             if(it==1 or it%output_freq==0 or it>=ntmax or isStationary or time >=tmax):
                 print"-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt)
-                if(isImplicit):
-                    print "Linear system converged in ", LS.getNumberOfIter(), " GMRES iterations"
+                print "Linear system converged in ", LS_staggered.getNumberOfIter(), " GMRES iterations"
     
-                np.savetxt("WaveSystem"+str(dim)+"DUpwind"   +meshName+"_pressure"+str(it)+".txt", pressure_field          , delimiter="\n")
-                np.savetxt("WaveSystem"+str(dim)+"DUpwind"   +meshName+"_velocity"+str(it)+".txt", velocity_field          , delimiter="\n")
                 np.savetxt("WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure"+str(it)+".txt", pressure_field_staggered, delimiter="\n")
                 np.savetxt("WaveSystem"+str(dim)+"DStaggered"+meshName+"_velocity"+str(it)+".txt", velocity_field_staggered, delimiter="\n")
-                np.savetxt("WaveSystem"+str(dim)+"DUpwindImplicit"   +meshName+"_pressure"+str(it)+".txt", pressure_field          , delimiter="\n")
-                np.savetxt("WaveSystem"+str(dim)+"DUpwindImplicit"   +meshName+"_velocity"+str(it)+".txt", velocity_field          , delimiter="\n")
-                plt.savefig("WaveSystem"+str(dim)+"DUpwindStaggered"+meshName+"_pressure"+str(it)+".png")
+                plt.savefig("WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure"+str(it)+".png")
     
                 print
     print"-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt)
@@ -262,9 +168,9 @@ def WaveSystemVF(ntmax, tmax, cfl, a,b,nx, output_freq, meshName,scaling):
         print "Régime stationnaire atteint au pas de temps ", it, ", t= ", time
         print "------------------------------------------------------------------------------------"
 
-        np.savetxt("WaveSystem"+str(dim)+"DUpwind"+meshName+"_pressure_Stat.txt", pressure_field, delimiter="\n")
-        np.savetxt("WaveSystem"+str(dim)+"DUpwind"+meshName+"_velocity_Stat.txt", velocity_field, delimiter="\n")
-        plt.savefig("WaveSystem"+str(dim)+"DUpwindStaggered"+meshName+"_pressure_Stat.png")
+        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure_Stat.txt", pressure_field_staggered, delimiter="\n")
+        np.savetxt( "WaveSystem"+str(dim)+"DStaggered"+meshName+"_velocity_Stat.txt", velocity_field_staggered, delimiter="\n")
+        plt.savefig("WaveSystem"+str(dim)+"DStaggered"+meshName+"_pressure_Stat.png")
 
         return
     else:
@@ -274,7 +180,7 @@ def WaveSystemVF(ntmax, tmax, cfl, a,b,nx, output_freq, meshName,scaling):
 
 def solve( a,b,nx, meshName, scaling, meshType, cfl):
 
-    print "Resolution of the Wave system in dimension 1 on "+str(nx)+ " cells"
+    print "Resolution of the Wave system in dimension 1 on "+str(nx)+ " cells, staggered scheme"
     print "Initial data : ", "Riemann problem"
     print "Boundary conditions : ", "Neumann"
     print "Mesh name : ",meshName , ", ", nx, " cells"
